dfd197ca3ece4dd7e843c7dad1dbca7f7c7eebac
upxin <951973194@qq.com>
2018-04-04 15:11:18 +0800

    新增组织内容

--- src/components/basic-information/manage-organization/index.js
+++ src/components/basic-information/manage-organization/index.js
@@ -1,59 +1,145 @@
 /**
  * Created by hj on 2018/3/30.
  */
+import ManageUserService from '@/services/manageUser.service'
+
 export default {
   name: 'app',
   data() {
     return {
-      columns4: [
+      modal: false,
+      currentLineInfo: false,
+      ruleOrg: {
+        code: [{ required: true, message: '请输入代码', trigger: 'blur' }],
+        fullName: [{ required: true, message: '请输入全称', trigger: 'blur' }],
+        abbreviation: [{ required: true, message: '请输入简称', trigger: 'blur' }],
+        scale: [{ required: true, message: '请输入规模', trigger: 'blur' }],
+        sample: [{ required: true, message: '请输入抽样率', trigger: 'blur' }],
+        redundancy: [{ required: true, message: '请输入冗余率', trigger: 'blur' }],
+        state: [{ required: true, message: '请输入状态', trigger: 'blur' }]
+      },
+      formOrganize: {
+        code: '',
+        fullName: '',
+        abbreviation: '',
+        scale: '',
+        sample: '',
+        redundancy: '',
+        state: ''
+      },
+      formOrganizeLine: {
+        code: '',
+        fullName: '',
+        abbreviation: '',
+        scale: '',
+        sample: '',
+        redundancy: '',
+        state: ''
+      },
+      columns: [
         {
           type: 'selection',
           width: 60,
           align: 'center'
         },
         {
+          title: '代码',
+          key: 'code'
+        },
+        {
           title: '全称',
-          key: 'name'
+          key: 'fullName'
         },
         {
           title: '简称',
-          key: 'age'
+          key: 'abbreviation'
         },
         {
           title: '规模',
-          key: 'address'
+          key: 'scale'
+        },
+        {
+          title: '抽样率',
+          key: 'sample'
+        },
+        {
+          title: '冗余率',
+          key: 'redundancy'
+        },
+        {
+          title: '状态',
+          key: 'state'
         }
       ],
-      data1: [
+      data: [
         {
-          name: 'John Brown',
-          age: 18,
-          address: 'New York No. 1 Lake Park',
-          date: '2016-10-03'
+          code: '0',
+          fullName: 'nnn'
         },
         {
-          name: 'Jim Green',
-          age: 24,
-          address: 'London No. 1 Lake Park',
-          date: '2016-10-01'
-        },
-        {
-          name: 'Joe Black',
-          age: 30,
-          address: 'Sydney No. 1 Lake Park',
-          date: '2016-10-02'
-        },
-        {
-          name: 'Jon Snow',
-          age: 26,
-          address: 'Ottawa No. 2 Lake Park',
-          date: '2016-10-04'
+          code: '0',
+          fullName: 'nnn',
+          state: 'kai'
         }
-      ]
+      ],
+      selected: [],
+      loading: true
     }
   },
   created() {
   },
   methods: {
+    currentLine(info) {
+      this.currentLineInfo = true
+      this.formOrganizeLine = info
+    },
+    selectChange(selection) {
+      this.selected = selection
+    },
+    loadOrglist() {
+      ManageUserService.getUsers()
+        .then((res) => {
+          this.data = res
+        })
+        .catch(() => {
+          this.$Message.error('获取用户列表失败！')
+        })
+    },
+    deleteOrgs() {
+      ManageUserService.deteleUser(this.selected)
+        .then(() => {
+          this.$Message.success('用户已删除！')
+        })
+        .catch(() => {
+          this.$Message.error('删除用户失败！')
+        })
+    },
+    handleCancel(name) {
+      this.$refs[name].resetFields()
+    },
+    handleCancelLine(name) {
+      this.$refs[name].resetFields()
+    },
+    handleConfirmLine(info) {
+      console.log(0)
+    },
+    handleConfirm(name) {
+      const self = this
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          ManageUserService.addUser(self.formOrganize)
+            .then(() => {
+              self.loading = false
+            })
+            .catch((error) => {
+              if (error.response.status === 409) {
+                self.$Message.error('用户已存在！')
+              } else {
+                self.$Message.error('增加用户失败！')
+              }
+            })
+        }
+      })
+    }
   }
 }
--- src/components/basic-information/manage-organization/index.vue
+++ src/components/basic-information/manage-organization/index.vue
@@ -7,20 +7,88 @@
     <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
       <div class="table-container">
         <div class="table-toolbar">
-          <Button  type="primary">新建</Button>
-          <Button  type="primary">删除</Button>
-          <Button  type="primary">刷新</Button>
-          <i-input placeholder="按订单搜索"  icon="ios-search" class="search_input pull-right" style="width: 200px">
+          <Button  type="primary" @click="modal = true">新建</Button>
+          <Button  type="primary" :disabled = !Boolean(selected.length) @click="deleteOrgs">删除</Button>
+          <Button  type="primary" @click="loadOrglist">刷新</Button>
+          <i-input placeholder="搜索组织"  icon="ios-search" class="search_input pull-right" style="width: 200px">
           </i-input>
         </div>
         <div>
-          <Table border ref="selection" :columns="columns4" :data="data1"></Table>
+          <Table
+          @on-selection-change="selectChange"
+          border ref="selection"
+          :columns="columns"
+          :data="data"
+          @on-row-click="currentLine"
+          ></Table>
         </div>
         <div class="table-page">
 
         </div>
       </div>
     </Content>
+    <Modal
+      v-model="currentLineInfo"
+      title="新建组织"
+      :loading="loading"
+      width="600"
+      @on-ok="handleConfirmLine('formOrganizeLine')"
+      @on-cancel="handleCancelLine('formOrganizeLine')">
+      <Form ref="formOrganize" :model="formOrganizeLine" :rules="ruleOrg" :label-width="80">
+        <FormItem label="代码" prop="code">
+          <Input type="text" v-model="formOrganizeLine.code"></Input>
+        </FormItem>
+        <FormItem label="全称" prop="fullName">
+          <Input type="text" v-model="formOrganizeLine.fullName"></Input>
+        </FormItem>
+        <FormItem label="简称" prop="abbreviation">
+          <Input type="text" v-model="formOrganizeLine.abbreviation"></Input>
+        </FormItem>
+        <FormItem label="规模" prop="scale">
+          <Input type="text" v-model="formOrganizeLine.scale" number></Input>
+        </FormItem>
+        <FormItem label="抽样率" prop="sample">
+          <Input type="text" v-model="formOrganizeLine.sample"></Input>
+        </FormItem>
+        <FormItem label="冗余率" prop="redundancy">
+          <Input type="text" v-model="formOrganizeLine.redundancy"></Input>
+        </FormItem>
+        <FormItem label="状态" prop="state">
+          <Input type="text" v-model="formOrganizeLine.state"></Input>
+        </FormItem>
+      </Form>
+    </Modal>
+    <Modal
+      v-model="modal"
+      title="新建组织"
+      :loading="loading"
+      width="600"
+      @on-ok="handleConfirm('formOrganize')"
+      @on-cancel="handleCancel('formOrganize')">
+      <Form ref="formOrganize" :model="formOrganize" :rules="ruleOrg" :label-width="80">
+        <FormItem label="代码" prop="code">
+          <Input type="text" v-model="formOrganize.code"></Input>
+        </FormItem>
+        <FormItem label="全称" prop="fullName">
+          <Input type="text" v-model="formOrganize.fullName"></Input>
+        </FormItem>
+        <FormItem label="简称" prop="abbreviation">
+          <Input type="text" v-model="formOrganize.abbreviation"></Input>
+        </FormItem>
+        <FormItem label="规模" prop="scale">
+          <Input type="text" v-model="formOrganize.scale" number></Input>
+        </FormItem>
+        <FormItem label="抽样率" prop="sample">
+          <Input type="text" v-model="formOrganize.sample"></Input>
+        </FormItem>
+        <FormItem label="冗余率" prop="redundancy">
+          <Input type="text" v-model="formOrganize.redundancy"></Input>
+        </FormItem>
+        <FormItem label="状态" prop="state">
+          <Input type="text" v-model="formOrganize.state"></Input>
+        </FormItem>
+      </Form>
+    </Modal>
   </div>
 </template>
 
@@ -29,4 +97,3 @@
 <style>
 
 </style>
-
