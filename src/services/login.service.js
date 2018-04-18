fd21348a9394b027e4522acf592e59313df87416
hejiang <2386488983@qq.com>
2018-04-18 17:54:39 +0800

    [ADD]

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -2,21 +2,22 @@
  * Created by hj on 2018/4/18.
  */
 import api from './api.service'
+import qs from 'qs'
 
 class LoginService {
   constructor() {
     this.api = api
   }
   sendSmsPwd(params) {
-    return this.api.post('/auth/sendSmsPwd', params)
+    return this.api.post('/auth/sendSmsPwd', qs.stringify(params))
   }
 
   Login(params) {
-    return this.api.post('/auth/login', params)
+    return this.api.post('/auth/login', qs.stringify(params))
   }
 
   Logout(params) {
-    return this.api.post('/auth/logout', params)
+    return this.api.post('/auth/logout', qs.stringify(params))
   }
 }
 
