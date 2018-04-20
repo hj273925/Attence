dfbe8a47371f140bd6259bb79854785ed50e0c2c
backend <http://gitlab.gallopdata.com/survey/backend.git>
2018-04-20 17:56:02 +0800

    [duan]

--- src/components/console/research-information/index.vue
+++ src/components/console/research-information/index.vue
@@ -6,7 +6,7 @@
     </Breadcrumb>
     <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
       <div class="table-container">
-        <Form :model="formCustom" label-position="left" :label-width="100">
+        <Form label-position="left" :label-width="100">
           <FormItem label="问卷">
             <Row>
               <Col span="2">
@@ -18,10 +18,14 @@
             </Row>
           </FormItem>
           <FormItem label="其它参数">
-            <span>Some Text</span>
+            <span>计划开始时间</span>
+            <span>计划结束时间</span>
+            <span>是否自动催填</span>
+            <span>催填间隔</span>
+            <span>状态</span>
           </FormItem>
           <FormItem label="筛选参数">
-            <Table border stripe :columns="columns" :data="data"></Table>
+            <Table :columns="columns" :data="rows"></Table>
           </FormItem>
         </Form>
       </div>
